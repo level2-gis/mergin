0fa698637f74bb7d138d9c2064430ce3f177b1de
Uroš Preložnik <uros00@gmail.com>
2025-08-05 22:12:02 +0200

    revert

--- web-app/packages/admin-app/src/modules/user/views/index.ts
+++ web-app/packages/admin-app/src/modules/user/views/index.ts
@@ -2,5 +2,4 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-MerginMaps-Commercial
 
-// export { default as LoginView } from './LoginView.vue'
-export { default as LoginView } from './CustomLoginWrapper.vue'
+export { default as LoginView } from './LoginView.vue'
